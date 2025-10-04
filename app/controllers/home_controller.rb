e957ab9270d22464683f53a47b71bc6b11c0abad
takumi <sakatagintoki78013@gmail.com>
2025-10-05 02:20:51 +0900

    ニュースのタイトルと内容を取得可能

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,25 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
-  def index; end
+  def index
+    @news = News.find_by(date: Date.today)
+
+    unless @news
+      news_data = NewsFetcher.new.fetch_daily_news
+
+      @news = if news_data
+                News.create(
+                  date: Date.today,
+                  title: news_data["title"],
+                  summary: news_data["summary"]
+                )
+              else
+                News.create(
+                  date: Date.today,
+                  title: "取得できていないかも",
+                  summary: "やっぱ取得できていない"
+                )
+              end
+    end
+  end
 end
